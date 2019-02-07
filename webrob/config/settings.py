f2e6f839810b2f4cb5f358920526f50c1dff0d45
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-07 12:24:55 +0100

    refactor(settings.py): refactor to use env_var_getter-module instead of os.environ.get

--- webrob/config/settings.py
+++ webrob/config/settings.py
@@ -1,15 +1,6 @@
-import os
 import webrob.utility.environment_variable_getter as evg
 
 DEV_SECRET_KEY='\\\xf8\x12\xdc\xf5\xb2W\xd4Lh\xf5\x1a\xbf"\x05@Bg\xdf\xeb>E\xd8<'
-
-# TODO: Code in comment can possibly be removed
-# = 'postgresql://docker@' + \
-#     os.environ['POSTGRES_PORT_5432_TCP_ADDR'] + ':' + \
-#     os.environ['POSTGRES_PORT_5432_TCP_PORT'] + '/docker'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://docker@' + \
-#    evg.get_variable_with_default('POSTGRES_PORT_5432_TCP_ADDR', 'localhost') + ':' + \
-#    evg.get_variable_with_default('POSTGRES_PORT_5432_TCP_PORT', '5432') + '/docker'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://docker@postgres_db:5432/docker'
 #SQLALCHEMY_ECHO = True
@@ -17,19 +8,19 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://docker@postgres_db:5432/docker'
 CSRF_ENABLED = True
 
 # email server
-MAIL_SERVER   = os.environ.get('OPENEASE_MAIL_SERVER', 'smtp.gmail.com')
-MAIL_PORT     = int(os.environ.get('OPENEASE_MAIL_PORT', '465'))
-MAIL_USE_TLS  = bool(os.environ.get('OPENEASE_MAIL_USE_TLS', 'False'))
-MAIL_USE_SSL  = bool(os.environ.get('OPENEASE_MAIL_USE_SSL', 'True'))
-MAIL_USERNAME = os.environ.get('OPENEASE_MAIL_USERNAME')
-# TODO: Possibly remove default return. Also possibly change all environ.gets to use our module environment-variable-getter
-MAIL_PASSWORD = os.environ.get('OPENEASE_MAIL_PASSWORD', '12345678')
+MAIL_SERVER   = evg.get_variable_with_default('OPENEASE_MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT     = int(evg.get_variable_with_default('OPENEASE_MAIL_PORT', '465'))
+MAIL_USE_TLS  = bool(evg.get_variable_with_default('OPENEASE_MAIL_USE_TLS', 'False'))
+MAIL_USE_SSL  = bool(evg.get_variable_with_default('OPENEASE_MAIL_USE_SSL', 'True'))
+MAIL_USERNAME = evg.get_variable_with_default_none('OPENEASE_MAIL_USERNAME')
+# TODO: Possibly remove default returns
+MAIL_PASSWORD = evg.get_variable_with_default('OPENEASE_MAIL_PASSWORD', '12345678')
 MAIL_DEFAULT_SENDER = '"Sender" <openease.iai@gmail.com>'
 
-FACEBOOK_APP_TOKENS = (os.environ.get('FACEBOOK_APP_ID'), os.environ.get('FACEBOOK_APP_SECRET'))
-TWITTER_APP_TOKENS  = (os.environ.get('TWITTER_APP_ID'), os.environ.get('TWITTER_APP_SECRET'))
-GITHUB_APP_TOKENS   = (os.environ.get('GITHUB_APP_ID'), os.environ.get('GITHUB_APP_SECRET'))
-GOOGLE_APP_TOKENS   = (os.environ.get('GOOGLE_APP_ID'), os.environ.get('GOOGLE_APP_SECRET'))
+FACEBOOK_APP_TOKENS = (evg.get_variable_with_default_none('FACEBOOK_APP_ID'), evg.get_variable_with_default_none('FACEBOOK_APP_SECRET'))
+TWITTER_APP_TOKENS  = (evg.get_variable_with_default_none('TWITTER_APP_ID'), evg.get_variable_with_default_none('TWITTER_APP_SECRET'))
+GITHUB_APP_TOKENS   = (evg.get_variable_with_default_none('GITHUB_APP_ID'), evg.get_variable_with_default_none('GITHUB_APP_SECRET'))
+GOOGLE_APP_TOKENS   = (evg.get_variable_with_default_none('GOOGLE_APP_ID'), evg.get_variable_with_default_none('GOOGLE_APP_SECRET'))
 
 USER_ENABLE_USERNAME = True
 USER_ENABLE_EMAIL = True
@@ -38,6 +29,6 @@ USER_ENABLE_CONFIRM_EMAIL = False
 MAX_HISTORY_LINES = 100
 
 MESH_REPOSITORIES = map(lambda x: tuple(x.split(' ')),
-                        os.getenv('OPENEASE_MESHES', 'git https://github.com/PR2/pr2_common').split(','))
+                        evg.get_variable_with_default('OPENEASE_MESHES', 'git https://github.com/PR2/pr2_common').split(','))
 
-ROS_DISTRIBUTION = os.getenv('OPENEASE_ROS_DISTRIBUTION')
+ROS_DISTRIBUTION = evg.get_variable_with_default_none('OPENEASE_ROS_DISTRIBUTION')
