d58d5c4d7bb8f4cf41ab4e2c68b9cebaa1887424
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-07 12:20:05 +0100

    feat(environment_variable_getter.py): add get_variable_with_default_none() to env_var_getter-module

--- webrob/utility/environment_variable_getter.py
+++ webrob/utility/environment_variable_getter.py
@@ -12,3 +12,6 @@ def get_variable_with_default(var_name, default):
         return get_required_variable(var_name)
     except KeyError:
         return default
+
+def get_variable_with_default_none(var_name):
+    return get_variable_with_default(var_name, None)
