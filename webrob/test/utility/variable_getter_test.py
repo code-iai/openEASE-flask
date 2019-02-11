c536527af62a43d95faf95f88cc5cd338ed54c38
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-11 15:56:30 +0100

    refactor(variable_getter_test.py): refactor imports to use 'from ... import ...'

--- webrob/test/utility/variable_getter_test.py
+++ webrob/test/utility/variable_getter_test.py
@@ -1,6 +1,7 @@
 import pytest
 import os
-import webrob.utility.environment_variable_getter as evg
+from webrob.utility.environment_variable_getter import get_required_variable, get_variable_with_default_none, \
+    get_variable_with_default
 
 ENVIRONMENT_VARIABLE_NAME = 'ENV1'
 ENVIRONMENT_VARIABLE_VALUE = 'myval'
@@ -8,24 +9,24 @@ ENVIRONMENT_VARIABLE_DEFAULT_VALUE = 'default'
 
 def test_successfully_get_required_variable(monkeypatch):
     monkeypatch.setitem(os.environ, ENVIRONMENT_VARIABLE_NAME, ENVIRONMENT_VARIABLE_VALUE)
-    assert evg.get_required_variable(ENVIRONMENT_VARIABLE_NAME) == ENVIRONMENT_VARIABLE_VALUE
+    assert get_required_variable(ENVIRONMENT_VARIABLE_NAME) == ENVIRONMENT_VARIABLE_VALUE
 
 def test_required_variable_exception():
     with pytest.raises(KeyError, match=ENVIRONMENT_VARIABLE_NAME):
-        evg.get_required_variable(ENVIRONMENT_VARIABLE_NAME)
+        get_required_variable(ENVIRONMENT_VARIABLE_NAME)
 
 def test_get_variable_with_default(monkeypatch):
     monkeypatch.setitem(os.environ, ENVIRONMENT_VARIABLE_NAME, ENVIRONMENT_VARIABLE_VALUE)
-    assert evg.get_variable_with_default(
+    assert get_variable_with_default(
         ENVIRONMENT_VARIABLE_NAME,
         ENVIRONMENT_VARIABLE_DEFAULT_VALUE) == ENVIRONMENT_VARIABLE_VALUE
 
 def test_get_default_value():
-    assert evg.get_variable_with_default(
+    assert get_variable_with_default(
         ENVIRONMENT_VARIABLE_NAME,
         ENVIRONMENT_VARIABLE_DEFAULT_VALUE) == ENVIRONMENT_VARIABLE_DEFAULT_VALUE
 
 def test_get_variable_with_default_None(monkeypatch):
-    assert evg.get_variable_with_default_none(ENVIRONMENT_VARIABLE_NAME) == None
+    assert get_variable_with_default_none(ENVIRONMENT_VARIABLE_NAME) == None
     monkeypatch.setitem(os.environ, ENVIRONMENT_VARIABLE_NAME, ENVIRONMENT_VARIABLE_VALUE)
-    assert evg.get_variable_with_default_none(ENVIRONMENT_VARIABLE_NAME) == ENVIRONMENT_VARIABLE_VALUE
+    assert get_variable_with_default_none(ENVIRONMENT_VARIABLE_NAME) == ENVIRONMENT_VARIABLE_VALUE
