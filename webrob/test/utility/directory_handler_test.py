496c9f8b3a3e243cebdc2e7aa2403116935cffb2
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-11 16:01:06 +0100

    test(*): add tests for file_handler and directory_handler

--- webrob/test/utility/directory_handler_test.py
+++ webrob/test/utility/directory_handler_test.py
@@ -1,9 +1,63 @@
 import os
+import shutil
 import pytest
 
-# TODO: figure out how to use the filebase without issues, because it depends on
-# both directory_ and file_handler currently
-import webrob.test.utility.testbase_file_io
-import webrob.utility.directory_handler
+from webrob.test.utility.testbase_file_io import TEMP_DIR_PATH
+from webrob.utility.path_builder import join as join_paths
+from webrob.utility.path_exists_checker import exists as path_exists
+from webrob.utility.directory_handler import rm_nonempty_dir, make_dirs, rm_empty_dir, mk_dir
 
-# TODO
+TEST_DIR = join_paths(TEMP_DIR_PATH, 'test')
+TEST_DIR_NESTED = join_paths(TEST_DIR, 'test')
+
+
+# both setup_module() and teardown_module() have to use the os-module
+# instead of the directory_handler as it's being tested in this module
+def setup_module():
+    remove_directory_if_exists(TEMP_DIR_PATH)
+    os.mkdir(TEMP_DIR_PATH)
+
+
+def remove_directory_if_exists(path):
+    if path_exists(path):
+        shutil.rmtree(path)
+
+
+def teardown_module():
+    shutil.rmtree(TEMP_DIR_PATH)
+
+
+# -------------------------------TESTS---------------------------------
+
+
+def test_mk_dir():
+    mk_dir(TEST_DIR)
+    assert path_exists(TEST_DIR) is True
+
+
+def test_make_dirs():
+    make_dirs(TEST_DIR_NESTED)
+    assert path_exists(TEST_DIR_NESTED) is True
+
+
+def test_rm_empty_dir():
+    assert_remove_function(TEST_DIR, rm_empty_dir)
+    assert_remove_function(TEST_DIR, rm_nonempty_dir)
+
+
+def assert_remove_function(path, remove_function, check_path = None):
+    remove_directory_if_exists(path)
+    make_dirs(path)
+
+    if check_path is None:      # path created and to check are the same
+        remove_function(path)
+        assert path_exists(path) is False
+    else:                       # path created and to check are different
+        remove_function(check_path)
+        assert path_exists(check_path) is False
+
+
+def test_rm_nonempty_dir():
+    with pytest.raises(OSError):
+        assert_remove_function(TEST_DIR_NESTED, rm_empty_dir, TEST_DIR)
+    assert_remove_function(TEST_DIR_NESTED, rm_nonempty_dir, TEST_DIR)
--- webrob/test/utility/file_handler_test.py
+++ webrob/test/utility/file_handler_test.py
@@ -1,9 +1,82 @@
 import os
 import pytest
 
-# TODO: figure out how to use the filebase without issues, because it depends on
-# both directory_ and file_handler currently
-import webrob.test.utility.testbase_file_io
-import webrob.utility.file_handler
+from webrob.test.utility.testbase_file_io import delete_temp_dir, TEMP_FILE_WITH_CONTENT_PATH, create_temp_dir, \
+    TEMP_FILE_CONTENT, EMPTY_TEMP_FILE_PATH, NOT_EXISTING_FILE
+from webrob.utility.path_exists_checker import exists as path_exists
+from webrob.utility.file_handler import create_file, remove_file, read_file, write_to_file
 
-# TODO
+
+def setup_module():
+    create_temp_dir()
+
+
+def teardown_module():
+    delete_temp_dir()
+
+
+# both setup_function() and teardown_function() have to use python
+# file read/write-functions instead of the file_handler as it's
+# being tested in this module
+def setup_function():
+    create_file(EMPTY_TEMP_FILE_PATH)
+
+
+def create_file(path, content=None):
+    dst_f = open(path, 'w+')
+    if content is not None:
+        dst_f.write(content)
+    dst_f.close()
+
+
+def teardown_function():
+    try:                # need try-except because some unit-tests remove the temp-file
+        os.remove(EMPTY_TEMP_FILE_PATH)
+    except OSError:
+        return
+
+
+# -------------------------------TESTS---------------------------------
+
+
+def test_read_existing_file():
+    create_file(TEMP_FILE_WITH_CONTENT_PATH, TEMP_FILE_CONTENT)
+    assert read_file(TEMP_FILE_WITH_CONTENT_PATH) == TEMP_FILE_CONTENT
+    remove_file(TEMP_FILE_WITH_CONTENT_PATH)
+
+
+def test_read_not_existing_file():
+    with pytest.raises(IOError):
+        read_file(NOT_EXISTING_FILE)
+
+
+def test_write_to_existing_file():
+    write_to_file(EMPTY_TEMP_FILE_PATH, TEMP_FILE_CONTENT)
+    assert read_file(EMPTY_TEMP_FILE_PATH) == TEMP_FILE_CONTENT
+
+
+def test_write_to_not_existing_file():
+    with pytest.raises(IOError):
+        write_to_file(NOT_EXISTING_FILE, TEMP_FILE_CONTENT)
+
+
+def test_create_file():
+    create_file(TEMP_FILE_WITH_CONTENT_PATH, TEMP_FILE_CONTENT)
+    assert path_exists(TEMP_FILE_WITH_CONTENT_PATH) is True
+    assert read_file(TEMP_FILE_WITH_CONTENT_PATH) == TEMP_FILE_CONTENT
+    remove_file(TEMP_FILE_WITH_CONTENT_PATH)
+
+
+def test_create_existing_file():
+    with pytest.raises(IOError):
+        create_file(EMPTY_TEMP_FILE_PATH, TEMP_FILE_CONTENT)
+
+
+def test_remove_existing_file():
+    remove_file(EMPTY_TEMP_FILE_PATH)
+    assert path_exists(EMPTY_TEMP_FILE_PATH) is False
+
+
+def test_remove_not_existing_file():
+    with pytest.raises(OSError):
+        remove_file(NOT_EXISTING_FILE)
