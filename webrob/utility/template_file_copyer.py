0c547f81210c78edb16c390a2b306fab5613294f
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-11 16:03:47 +0100

    fix(template_file_copyer.py): fix and refactor module

--- webrob/utility/template_file_copyer.py
+++ webrob/utility/template_file_copyer.py
@@ -1,17 +1,21 @@
 import os
 
-from webrob.utility.file_handler import read_file, write_file
+from webrob.utility.directory_handler import make_dirs
+from webrob.utility.file_handler import read_file, create_file
+from webrob.utility.path_exists_checker import exists as path_exists
 
-#TODO
 
 def copy_template_file(src, dst, args):
     template = read_file(src)
     create_parent_dir(dst)
     copy_file_and_replace_keywords(dst, template, args)
 
+
 def create_parent_dir(dst):
     parent = os.path.dirname(dst)
-    if not os.path.exists(parent):  os.makedirs(parent)
+    if not path_exists(parent):
+        make_dirs(parent)
+
 
 def copy_file_and_replace_keywords(dst, template, args):
-    write_file(dst, template % args)
+    create_file(dst, template.format(*args))
