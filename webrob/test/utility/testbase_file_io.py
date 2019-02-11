9376d23e671fbf7430961acef723a85bc4ba886d
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-11 15:59:50 +0100

    feat(testbase_file_io.py): fix bugs, refactor module & add new functions

--- webrob/test/utility/testbase_file_io.py
+++ webrob/test/utility/testbase_file_io.py
@@ -1,21 +1,35 @@
-import webrob.utility.directory_handler as dir_handler
-import webrob.utility.file_handler as file_handler
+from webrob.utility.path_exists_checker import exists as path_exists
+from webrob.utility.directory_handler import rm_nonempty_dir, mk_dir
+from webrob.utility.file_handler import create_file, remove_file
 
-TEMP_DIR_PATH = ''
-TEMP_FILE_PATH = ''
-
-
-def setup_module():
-    dir_handler.mk_dir(TEMP_DIR_PATH)
-
-
-def teardown_module():
-    dir_handler.rm_dir(TEMP_DIR_PATH)
+TEMP_DIR_PATH = '../temp'
+TEMP_FILE_WITH_CONTENT_PATH = '../temp/not-empty.txt'
+TEMP_FILE_CONTENT = 'something'
+EMPTY_TEMP_FILE_PATH = '../temp/empty.txt'
+NOT_EXISTING_FILE = '../temp/nothing.txt'
 
 
-def setup_function():
-    file_handler.create_file(TEMP_FILE_PATH)
+def create_temp_dir():
+    if path_exists(TEMP_DIR_PATH):
+        rm_nonempty_dir(TEMP_DIR_PATH)
+    mk_dir(TEMP_DIR_PATH)
 
 
-def teardown_function():
-    file_handler.remove_file(TEMP_FILE_PATH)
+def delete_temp_dir():
+    rm_nonempty_dir(TEMP_DIR_PATH)
+
+
+def create_empty_temp_file():
+    create_file(EMPTY_TEMP_FILE_PATH)
+
+
+def delete_empty_temp_file():
+    remove_file(EMPTY_TEMP_FILE_PATH)
+
+
+def create_temp_file_with_content():
+    create_file(TEMP_FILE_WITH_CONTENT_PATH, TEMP_FILE_CONTENT)
+
+
+def delete_temp_file_with_content():
+    remove_file(TEMP_FILE_WITH_CONTENT_PATH)
