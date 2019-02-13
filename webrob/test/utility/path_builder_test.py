005d0576154f50e4863b8c4975dc944eee83410d
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-13 13:06:18 +0100

    refactor(path_builder_test.py): refactor tests to not have duplicate code

--- webrob/test/utility/path_builder_test.py
+++ webrob/test/utility/path_builder_test.py
@@ -8,15 +8,20 @@ def test_join_paths():
     path2 = 'second'
     path3 = 'third'
 
-    result_path1 = '{0}'.format(path1)
-    result_path2 = '{0}/{1}'.format(path1, path2)
-    result_path3 = '{0}/{1}/{2}'.format(path1, path2, path3)
+    join_paths_and_assert_correctness(path1)
+    join_paths_and_assert_correctness(path1, path2)
+    join_paths_and_assert_correctness(path1, path2, path3)
 
-    builder_result_path1 = join_paths(path1)
-    builder_result_path2 = join_paths(path1, path2)
-    builder_result_path3 = join_paths(path1, path2, path3)
 
-    # replace is needed so tests run on uniformly on all OS
-    assert builder_result_path1.replace(os.sep, '/') == result_path1
-    assert builder_result_path2.replace(os.sep, '/') == result_path2
-    assert builder_result_path3.replace(os.sep, '/') == result_path3
+def join_paths_and_assert_correctness(*args):
+    template_string = build_template_string(len(args))
+    result_path = template_string.format(*args)
+    builder_result_path = join_paths(*args)
+    assert builder_result_path.replace(os.sep, '/') == result_path  # replace is needed so tests run on uniformly on all OS
+
+
+def build_template_string(number_of_placeholders):
+    template = ''
+    for x in xrange(number_of_placeholders):
+        template += '{{0}}/'.format(str(x))
+    return template[0:len(template)-1]  # removes final slash
