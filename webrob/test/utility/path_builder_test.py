196fb423f9949f9a1950c7ca2bb576d68fc681f5
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-13 17:34:49 +0100

    fix(path_builder_test.py): fix build_template_string function

--- webrob/test/utility/path_builder_test.py
+++ webrob/test/utility/path_builder_test.py
@@ -15,13 +15,16 @@ def test_join_paths():
 
 def join_paths_and_assert_correctness(*args):
     template_string = build_template_string(len(args))
-    result_path = template_string.format(*args)
+    expected_result_path = template_string.format(*args)
     builder_result_path = join_paths(*args)
-    assert builder_result_path.replace(os.sep, '/') == result_path  # replace is needed so tests run on uniformly on all OS
+    assert builder_result_path.replace(os.sep, '/') == expected_result_path  # replace is needed so tests run on uniformly on all OS
 
 
 def build_template_string(number_of_placeholders):
     template = ''
     for x in xrange(number_of_placeholders):
-        template += '{{0}}/'.format(str(x))
-    return template[0:len(template)-1]  # removes final slash
+        template += '{' + str(x) + '}'
+        if x < number_of_placeholders - 1:
+            template += '/'
+
+    return template
