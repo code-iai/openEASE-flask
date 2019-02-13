99694d45d7670df626cbd73ddfde4515122cbf5d
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-13 17:04:07 +0100

    fix(template_file_copyer.py): fix to work with current test suite

--- webrob/utility/template_file_copyer.py
+++ webrob/utility/template_file_copyer.py
@@ -1,3 +1,4 @@
+import re
 from webrob.utility.dir_name_getter import get_parent_dir_name
 from webrob.utility.directory_handler import make_dirs
 from webrob.utility.file_handler import read_file, create_file
@@ -6,15 +7,30 @@ from webrob.utility.path_exists_checker import exists as path_exists
 
 def copy_template_file(src, dst, args):
     template = read_file(src)
-    create_parent_dir(dst)
-    copy_file_and_replace_keywords(dst, template, args)
+    __create_parent_dir(dst)
+    __copy_file_and_replace_keywords(dst, template, args)
 
 
-def create_parent_dir(dst):
+def __create_parent_dir(dst):
     parent = get_parent_dir_name(dst)
     if not path_exists(parent):
         make_dirs(parent)
 
 
-def copy_file_and_replace_keywords(dst, template, args):
-    create_file(dst, template.format(*args))
+def __copy_file_and_replace_keywords(dst, template, args):
+    content = __format_template(template, args)
+    create_file(dst, content)
+
+
+def __format_template(template, args):
+    if __get_number_of_template_fillers(template) < len(args):
+        raise IndexError('number of arguments for str.format() is more than the number of fillers in the template')
+    elif __get_number_of_template_fillers(template) > len(args):
+        raise IndexError('number of arguments for str.format() is less than the number of fillers in the template')
+    return template.format(*args)
+
+
+def __get_number_of_template_fillers(template):
+    # find all occurences of {[integer number]} in template
+    string_matches = re.findall('({[0-9]+})', template)
+    return len(string_matches)
