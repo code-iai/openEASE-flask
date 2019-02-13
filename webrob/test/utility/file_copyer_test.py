4532c3e1d1a8efd57662a3fc6536f73114250ba7
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-13 17:03:01 +0100

    test(file_copyer_test.py): finish test suite for module

--- webrob/test/utility/file_copyer_test.py
+++ webrob/test/utility/file_copyer_test.py
@@ -1,22 +1,29 @@
-import os
 import pytest
 
-from webrob.test.utility.testbase_file_io import delete_temp_dir, delete_temp_file_with_content, \
-    TEMP_FILE_WITH_CONTENT_PATH, TEMP_DIR_PATH, create_temp_dir, create_temp_file_with_content
+from webrob.test.utility.testbase_file_io import delete_temp_dir, EMPTY_TEMP_FILE, TEMP_DIR, \
+    create_temp_dir, create_empty_temp_file
+from webrob.utility.dir_name_getter import get_parent_dir_name
+from webrob.utility.file_handler import read_file, write_to_file
 from webrob.utility.path_builder import join as join_paths
 from webrob.utility.path_exists_checker import exists as path_exists
-from webrob.utility.file_handler import read_file, write_to_file, remove_file
-from webrob.utility.template_file_copyer import copy_file_and_replace_keywords
+from webrob.utility.template_file_copyer import __copy_file_and_replace_keywords, __create_parent_dir, \
+    copy_template_file, __get_number_of_template_fillers
 
+BUILD_DESTINATION = join_paths(TEMP_DIR, 'copy.txt')
+PARENT_DIR = get_parent_dir_name(BUILD_DESTINATION)
+TEMPLATE_FILE = EMPTY_TEMP_FILE
 TEMPLATE_CONTENT = '{0} and {1}'
 KEYWORDS = ['apples', 'oranges']
+TOO_FEW_KEYWORDS = list(KEYWORDS).pop(1)
+TOO_MANY_KEYWORDS = list(KEYWORDS)
+TOO_MANY_KEYWORDS.append('pears')      # for some reason cannot do list(...).append(object), returns None
 EXPECTED_RESULT = '{0} and {1}'.format(KEYWORDS[0], KEYWORDS[1])
 
 
 def setup_function():
     create_temp_dir()
-    create_temp_file_with_content()
-    write_to_file(TEMP_FILE_WITH_CONTENT_PATH, TEMPLATE_CONTENT)
+    create_empty_temp_file()
+    write_to_file(TEMPLATE_FILE, TEMPLATE_CONTENT)
 
 
 def teardown_function():
@@ -27,20 +34,39 @@ def teardown_function():
 
 
 def test_copy_template_file():
-    return
+    copy_template_file(TEMPLATE_FILE, BUILD_DESTINATION, KEYWORDS)
+    assert path_exists(BUILD_DESTINATION) is True
+    assert read_file(BUILD_DESTINATION) == EXPECTED_RESULT
 
 
 def test_create_parent_dir():
-    return
+    # run twice, to see if errors are thrown if parent directory already exists
+    for x in xrange(2):
+        __create_parent_dir(BUILD_DESTINATION)
+        assert path_exists(PARENT_DIR) is True
 
 
 def test_copy_file_and_replace_keywords():
-    dst = join_paths(TEMP_DIR_PATH, 'copy.txt')
-    template = read_file(TEMP_FILE_WITH_CONTENT_PATH)
+    template = read_file(TEMPLATE_FILE)
 
-    copy_file_and_replace_keywords(dst, template, KEYWORDS)
+    __copy_file_and_replace_keywords(BUILD_DESTINATION, template, KEYWORDS)
 
-    assert path_exists(dst) is True
-    assert read_file(dst) == EXPECTED_RESULT
+    assert path_exists(BUILD_DESTINATION) is True
+    assert read_file(BUILD_DESTINATION) == EXPECTED_RESULT
 
-    remove_file(dst)
+
+def test_keyword_amount_does_not_match_template_fillers():
+    template = read_file(TEMPLATE_FILE)
+
+    with pytest.raises(IndexError):
+        __copy_file_and_replace_keywords(BUILD_DESTINATION, template, TOO_FEW_KEYWORDS)
+    with pytest.raises(IndexError):
+        __copy_file_and_replace_keywords(BUILD_DESTINATION, template, TOO_MANY_KEYWORDS)
+
+
+def test_get_number_of_template_fillers():
+    assert __get_number_of_template_fillers('{}') == 0
+    assert __get_number_of_template_fillers('{0}') == 1
+    assert __get_number_of_template_fillers('{0} and {1}') == 2
+    assert __get_number_of_template_fillers('{}{0} and {123}') == 2
+    assert __get_number_of_template_fillers('{}{0} and }{ {123}') == 2
