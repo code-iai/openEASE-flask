99c4ec0a371b47689ef70ee2ed2d9e11e94b6ee4
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-14 10:55:52 +0100

    test(path_exists_checker_test.py): Build directories to check in setup & delete them in teardown

--- webrob/test/utility/path_exists_checker_test.py
+++ webrob/test/utility/path_exists_checker_test.py
@@ -1,9 +1,24 @@
-import os
-
+from webrob.test.utility.testbase_file_io import TEMP_DIR
+from webrob.utility.directory_handler import rm_nonempty_dir, mk_dir
+from webrob.utility.path_builder import join as join_paths
 from webrob.utility.path_exists_checker import exists as path_exists
 
-EXISTING_PATH = os.path.abspath('../utility')
-NOT_EXISTING_PATH = os.path.abspath('../nothing')
+EXISTING_PATH = TEMP_DIR
+NOT_EXISTING_PATH = join_paths(TEMP_DIR, 'nothing')
+
+# cannot use testbase_file_io.create_temp() as it uses functionality of this module
+# instead use directory_handler and create and delete temp_dir manually
+def setup_module():
+    mk_dir(EXISTING_PATH)
+    return
+
+
+def teardown_module():
+    rm_nonempty_dir(EXISTING_PATH)
+    return
+
+
+# -------------------------------TESTS---------------------------------
 
 
 def test_path_exists():
