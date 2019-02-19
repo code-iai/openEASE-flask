5520f8b5bfb5f6017e025611a697ea2d579c3835
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-02-19 16:21:00 +0100

    refactor(init_app.py): refactor and split methods

--- webrob/startup/init_app.py
+++ webrob/startup/init_app.py
@@ -24,30 +24,15 @@ from webrob.models.users import Role, User
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-def add_user(app,db,user_manager,name,mail,pw,displayname='',remoteapp='',roles=[]):
+def add_user(app, db, user_manager, name, mail, pw, display_name='', remote_app='', roles=[]):
     if not __password_is_valid(app, name, pw):
         return
-    
-    user = User.query.filter(User.username==name).first()
-    if user:
-        return user
-    
-    user = User(username=name,
-                displayname=displayname,
-                remoteapp=remoteapp,
-                email=mail,
-                active=True,
-                password=user_manager.hash_password(pw),
-                confirmed_at=datetime.datetime.utcnow())
-    for r in roles:
-        x = Role.query.filter(Role.name==r).first()
-        if x==None:
-            app.logger.info("Unable to find role: " + str(r))
-        else:
-            user.roles.append(x)
-    db.session.add(user)
-    db.session.commit()
-    
+
+    user = __get_user_from_db(name)
+
+    if not user:
+        user = __create_new_user_and_add_to_db(app, db, user_manager, name, mail, pw, display_name, remote_app, roles)
+
     return user
 
 
@@ -61,20 +46,53 @@ def __password_is_valid(app, name, pw):
     return False
 
 
+def __get_user_from_db(name):
+    return User.query.filter(User.username == name).first()
+
+
+def __create_new_user_and_add_to_db(app, db, user_manager, name, mail, pw, display_name, remote_app, roles):
+    user = User(active=True,
+                username=name,
+                displayname=display_name,
+                remoteapp=remote_app,
+                email=mail,
+                confirmed_at=datetime.datetime.utcnow(),
+                password=user_manager.hash_password(pw))
+
+    user = __append_roles_to_user_object(app, user, roles)
+    __add_user_to_db(db, user)
+
+    return user
+
+
+def __append_roles_to_user_object(app, user, roles):
+    user_with_roles = user
+
+    for r in roles:
+        curr_role = __get_role_from_db(r)
+        if curr_role is None:
+            __log_role_query_failure(app, r)
+        else:
+            user_with_roles.roles.append(curr_role)
+
+    return user_with_roles
+
+
+def __get_role_from_db(role):
+    return Role.query.filter(Role.name == role).first()
+
+
+def __log_role_query_failure(app, role):
+    app.logger.info("Unable to find role: " + str(role))
+
+
+def __add_user_to_db(db, user):
+    db.session.add(user)
+    db.session.commit()
+
+
 def init_app(app, db_instance, extra_config_settings={}):
-    # Initialize app config settings
-    app.config.from_object('webrob.config.settings')        # Read config from 'app/settings.py' file
-    app.config.update(extra_config_settings)                # Overwrite with 'extra_config_settings' parameter
-    if app.testing:
-        app.config['WTF_CSRF_ENABLED'] = False              # Disable CSRF checks while testing
-    if evg.get_variable_with_default('EASE_DEBUG', 'ease_debug') == 'true':
-        app.config['DEBUG'] = True
-        app.config['SECRET_KEY'] = app.config['DEV_SECRET_KEY']
-    else:
-        try:
-            app.config['SECRET_KEY'] = open('/etc/ease_secret/secret', 'rb').read()
-        except IOError:
-            app.config['SECRET_KEY'] = random_string(64)
+    __init_app_config_settings(app, extra_config_settings)
 
     # Setup Flask-Mail
     mail = Mail(app)
@@ -84,8 +102,9 @@ def init_app(app, db_instance, extra_config_settings={}):
     # Setup Flask-User to handle user account related forms
     from webrob.models.users import User
     db_adapter = SQLAlchemyAdapter(db_instance, User)
-    app.user_manager = UserManager(db_adapter, app)     # Init Flask-User and bind to app
+    app.user_manager = UserManager(db_adapter, app)  # Init Flask-User and bind to app
 
+    # TODO: Find better solution than local imports
     # Load all models.py files to register db.Models with SQLAlchemy
     from webrob.models import users
     from webrob.models import tutorials
@@ -104,8 +123,8 @@ def init_app(app, db_instance, extra_config_settings={}):
     from webrob.pages import tutorials
     # TODO: Why does this throw errors?
     # from webrob.pages import oauth
-    
-    # init_db(app, db_instance)
+
+    init_db(app, db_instance)
     init_webapp(app, db_instance)
 
     add_user(app=app, db=db_instance, user_manager=app.user_manager,
@@ -114,11 +133,24 @@ def init_app(app, db_instance, extra_config_settings={}):
              pw=os.environ.get('OPENEASE_ADMIN_PASSWORD'),
              roles=['admin'])
 
-    # add_user(app=app,db=db_instance,user_manager=app.user_manager,
-    #          name='admin',
-    #          mail=evg.get_variable_with_default('OPENEASE_MAIL_USERNAME', 'admin@openease.org'),
-    #          pw=evg.get_variable_with_default('OPENEASE_ADMIN_PASSWORD', '12345678'),
-    #          roles=['admin'])
-
-    app.logger.info("Webapp started.")
+    __log_webapp_started(app)
     return app
+
+
+def __init_app_config_settings(app, extra_config_settings):
+    app.config.from_object('webrob.config.settings')  # Read config from 'app/settings.py' file
+    app.config.update(extra_config_settings)  # Overwrite with 'extra_config_settings' parameter
+    if app.testing:
+        app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF checks while testing
+    if evg.get_variable_with_default('EASE_DEBUG', 'false') == 'true':
+        app.config['DEBUG'] = True
+        app.config['SECRET_KEY'] = app.config['DEV_SECRET_KEY']
+    else:
+        try:
+            app.config['SECRET_KEY'] = open('/etc/ease_secret/secret', 'rb').read()
+        except IOError:
+            app.config['SECRET_KEY'] = random_string(64)
+
+
+def __log_webapp_started(app):
+    app.logger.info("Webapp started.")
