de94052b68d394da58cddb6f121004e7ff58b86b
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-04-15 12:56:46 +0200

    refactor(db.py): refactor code for better readability and less code duplication
    refactor code for better readability and less code duplication; in case
    this causes errors due to flask/werkzeuge locals or imports, please
    report to GitHub issues

--- webrob/pages/db.py
+++ webrob/pages/db.py
@@ -16,10 +16,19 @@ __author__ = 'danielb@cs.uni-bremen.de'
 
 @app.route('/db/docu/<key>', methods=['POST'])
 def db_docu_text(key):
+    docs = __get_all_docs()
+    for d in docs:  # find specific
+        if d.key == key:
+            return jsonify(result=d.text)
+    __display_warning_when_unable_to_find_docu(key)
+
+
+def __get_all_docs():
     db_adapter = current_app.user_manager.db_adapter
-    docs = db_adapter.find_all_objects(db_table_class('docu'))
-    for d in docs:
-        if d.key == key: return jsonify(result=d.text)
+    return db_adapter.find_all_objects(db_table_class('docu'))
+
+
+def __display_warning_when_unable_to_find_docu(key):
     app.logger.warn("Unable to find docu for: " + str(key) + ".")
     abort(404)
 
@@ -33,10 +42,9 @@ def db_page_user_roles():
 @app.route('/db/find/user_roles', methods=['POST'])
 @admin_required
 def db_find_user_roles():
-    db_adapter = current_app.user_manager.db_adapter
-    users = db_adapter.find_all_objects(db_table_class('user'))
+    users = __get_all_users()
     user_roles = []
-    for u in users:
+    for u in users:     # find user roles
         user_roles.append({
             'id': u.id,
             'name': u.username,
@@ -45,13 +53,23 @@ def db_find_user_roles():
     return jsonify(result=user_roles)
 
 
+def __get_all_users():
+    db_adapter = current_app.user_manager.db_adapter
+    return db_adapter.find_all_objects(db_table_class('user'))
+
+
 @app.route('/db/save/user_roles', methods=['POST'])
 @admin_required
 def db_save_user_roles():
     data = json.loads(request.data)
-    if 'roles' not in data or 'id' not in data:
+    if 'roles' in data and 'id' in data:
+        __save_roles_to_users_and_commit_to_db(data)
+    else:
         app.logger.warn("Invalid input data.")
-        return jsonify(result=None)
+    return jsonify(result=None)
+
+
+def __save_roles_to_users_and_commit_to_db(data):
     db_adapter = current_app.user_manager.db_adapter
     user = db_find(db_table_class('user'), data['id'])
     for r in data['roles']:
@@ -59,7 +77,6 @@ def db_save_user_roles():
         if role is not None:
             user.roles.append(role)
     db_adapter.commit()
-    return jsonify(result=None)
 
 
 @app.route('/db/page/<table>')
